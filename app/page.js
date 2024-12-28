c3e886e6a0e26d0476edf156c98c06495b9cbfec
Kumail Ahmad <kumailahmad217@gmail.com>
2024-12-28 20:51:13 +0530

    modification

--- app/page.js
+++ app/page.js
@@ -2,7 +2,7 @@ import Image from "next/image";
 
 export default function Home() {
   return (
-    <div classNameName="">
+    <div className="">
       <section className="text-gray-600 body-font">
         <div className="container mx-auto flex px-5 py-24 md:flex-row flex-col items-center">
           <div className="lg:flex-grow md:w-1/2 lg:pr-24 md:pr-16 flex flex-col md:items-start md:text-left mb-16 md:mb-0 items-center text-center">
